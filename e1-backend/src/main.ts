6a5acba35debfa3e30dc67f1fbd1ad3d56b026c1
Daniel-SebastianGaete <daniel.sebastian@uc.cl>
2024-04-15 12:45:34 -0400

    database created

--- e1-backend/src/main.ts
+++ e1-backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { sequelize } from './database/connection';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  try {
+    sequelize.authenticate()
+    console.log('Connection has been established successfully.');
+    await sequelize.sync({ force: true });
+
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 }
 bootstrap();
