fcbab7f761f9d7fa3d0b945920861cd57e2cd006
Daniel-SebastianGaete <daniel.sebastian@uc.cl>
2024-04-17 13:49:54 -0400

    feature: models

---
+++ API/src/db/models/User.ts
@@ -0,0 +1,26 @@
+import { Table, Column, Model, DataType, HasMany } from 'sequelize-typescript';
+import Request from './Request';
+
+@Table({
+    timestamps: true,
+    tableName: 'users',
+    modelName: 'User'
+})
+class User extends Model {
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    declare email: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    declare username: string;
+
+    @HasMany(() => Request)
+    declare requests: Request[];
+}
+
+export default User;
--- API/src/main.ts
+++ API/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import sequelize from './db/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to database has been established successfully.');
+    await sequelize.sync({ force: true });
+    console.log('All models were synchronized successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 }
+
 bootstrap();
