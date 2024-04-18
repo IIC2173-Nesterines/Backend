cc0b8edefa7b3aff6871bbac9ef19338169ba228
Beatriz <beatriz.errazuriz@uc.cl>
2024-04-17 23:48:22 -0400

    feat: :construction: setup sequelize and added user module

--- API/src/app.module.ts
+++ API/src/app.module.ts
@@ -1,10 +1,23 @@
+import "dotenv/config"
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    SequelizeModule.forRoot({
+      database: process.env.DATABASE_NAME,
+      dialect: 'postgres',
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      host: process.env.DATABASE_HOST,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UsersModule
+  ],
   providers: [AppService],
 })
+
 export class AppModule {}
--- API/src/db/models/User.ts
+++ API/src/db/models/User.ts
@@ -1,26 +1,28 @@
+// models/User.ts
+
 import { Table, Column, Model, DataType, HasMany } from 'sequelize-typescript';
 import Request from './Request';
 
 @Table({
-    timestamps: true,
-    tableName: 'users',
-    modelName: 'User'
+  timestamps: true,
+  tableName: 'users',
+  modelName: 'Users',
 })
 class User extends Model {
-    @Column({
-        type: DataType.STRING,
-        allowNull: false,
-    })
-    declare email: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare email: string;
 
-    @Column({
-        type: DataType.STRING,
-        allowNull: false
-    })
-    declare username: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare username: string;
 
-    @HasMany(() => Request)
-    declare requests: Request[];
+  @HasMany(() => Request)
+  declare requests: Request[];
 }
 
 export default User;
