008f170c7b0170861a879bb78525421a52f7b1b4
Joaquin Vinuela <joaquin.vinuela@uc.cl>
2024-04-19 16:49:46 -0400

    feat: :sparkles: Created Enpoints for Flights

--- API/src/app.module.ts
+++ API/src/app.module.ts
@@ -4,8 +4,6 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { AuthzModule } from './authz/authz.module';
 import { UsersModule } from './users/users.module';
 import { FlightsModule } from './flights/flights.module';
-import { AuthzModule } from './authz/authz.module';
-import { UsersModule } from './users/users.module';
 import sequelize from './db/config';
 
 @Module({
@@ -15,7 +13,6 @@ import sequelize from './db/config';
     AuthzModule,
     FlightsModule,
     ConfigModule.forRoot(),
-    AuthzModule,
   ],
 })
 export class AppModule {}
