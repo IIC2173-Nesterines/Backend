7a3ac2db5f6b92124055483ec01c899f28c7fcc9
Beatriz <beatriz.errazuriz@uc.cl>
2024-04-19 23:34:46 -0400

    fix: :ambulance: add cors for frontend integration

--- API/src/main.ts
+++ API/src/main.ts
@@ -11,7 +11,13 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(3001);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+  await app.listen(process.env.API_PORT || 3000);
 
   try {
     await sequelize.authenticate();
