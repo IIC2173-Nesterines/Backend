e624f3e3d903396174423d06335776a6c3d7c449
Beatriz <beatriz.errazuriz@uc.cl>
2024-04-20 10:56:49 -0400

    fix: :art: fix small errors

--- API/src/app.module.ts
+++ API/src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AuthzModule } from './authz/authz.module';
 import { UsersModule } from './users/users.module';
-// import { AppService } from './app.service';
 import { FlightsModule } from './flights/flights.module';
 import sequelize from './db/config';
 
@@ -11,9 +10,6 @@ import sequelize from './db/config';
   imports: [
     SequelizeModule.forRoot(sequelize.options),
     UsersModule,
-    AuthzModule,
-    FlightsModule,
-    ConfigModule.forRoot(),
     AuthzModule,
     FlightsModule,
     ConfigModule.forRoot(),
