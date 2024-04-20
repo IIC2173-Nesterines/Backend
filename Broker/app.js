86c52cf71501c76eaa0e0864cdc7e921d3be92c9
Diego Sfeir <dsfeir@uc.cl>
2024-04-20 15:11:32 -0400

    feat: Broker Subscriber to New Topics

--- Broker/app.js
+++ Broker/app.js
@@ -6,7 +6,7 @@ const HOST = process.env.HOST;
 const PORT = process.env.PORT;
 const USERNAME = process.env.USERNAME;
 const PASSWORD = process.env.PASSWORD;
-const api_url = process.env.API_URL;
+const post_flight_url = process.env.API_URL;
 
 
 const client = mqtt.connect({
@@ -19,32 +19,69 @@ const client = mqtt.connect({
 
 client.on('connect', () => {
     console.log('Connected');
-    client.subscribe('flights/info', (err) => {
+    client.subscribe('flights/info', (err) => { // Subscribe to the topic flights/info
         if (!err) {
             console.log('Subscribed to flights/info');
         }
     });
-});
-client.on('message', (topic, message) => {
-    message = message.toString();
-    fs.appendFile('log.txt', message + '\n', (err) => {
-        if (err) {
-            console.error('Error appending buffer to log.txt:', err);
+    client.subscribe('flights/requests', (err) => {
+        if (!err) {
+            console.log('Subscribed to flights/requests'); // Subscribe to the topic flights/requests
         }
     });
-    message = JSON.parse(message);
-    message.forEach(obj => {
-        obj.flights = JSON.parse(obj.flights);
-        obj.carbonEmission = JSON.parse(obj.carbonEmission);
+    client.subscribe('flights/validation', (err) => {
+        if (!err) {
+            console.log('Subscribed to flights/validation'); // Subscribe to the topic flights/validation
+        }
     });
-    console.log('Received message from topic:', message);
-    // axios.post(api_url, message)
-    //     .then(response => {
-    //         console.log('Response:', response.data);
-    //     })
-    //     .catch(error => {
-    //         console.error('Error:', error);
-    //     });
+});
+
+client.on('message', (topic, message) => {
+    if (topic === 'flights/info') {
+        message = message.toString();
+        fs.appendFile('log.txt', message + '\n', (err) => {
+            if (err) {
+                console.error('Error appending buffer to log.txt:', err);
+            }
+        });
+        message = JSON.parse(message);
+        message.forEach(obj => {
+            obj.flights = JSON.parse(obj.flights);
+            obj.carbonEmission = JSON.parse(obj.carbonEmission);
+        });
+        console.log('Received message from topic:', topic);
+        console.log('Message:', message);
+        axios.post(post_flight_url, message)
+            .then(response => {
+                console.log('Response:', response.data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    } else if (topic === 'flights/requests') {
+        message = message.toString();
+        fs.appendFile('log.txt', message + '\n', (err) => {
+            if (err) {
+                console.error('Error appending buffer to log.txt:', err);
+            }
+        });
+        message = JSON.parse(message);
+        console.log('Received message from topic:', topic);
+        console.log('Message:', message);
+        // TODO: Send message to API
+
+    } else if (topic === 'flights/validation') {
+        message = message.toString();
+        fs.appendFile('log.txt', message + '\n', (err) => {
+            if (err) {
+                console.error('Error appending buffer to log.txt:', err);
+            }
+        });
+        message = JSON.parse(message);
+        console.log('Received message from topic:', topic);
+        console.log('Message:', message);
+        // TODO: Send message to API
+    }
 });
 
 client.on('error', function (error) {
